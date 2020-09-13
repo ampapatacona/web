96ae4d8cc7952136d5cc8b32d4a645889a61b451
monjo <jvmonjo@gmail.com>
2020-09-13 21:46:39 +0200

    ADDED: pages cms

--- content/es/pages/quienes-somos.md
+++ content/es/pages/quienes-somos.md
@@ -1,1 +1,1 @@
-# Quienes somos
+
---
+++ pages/quisom.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h1>{{ page.title }}</h1>
+    <nuxt-content :document="page" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, app }) {
+    const defaultLocale = app.i18n.locale
+    const title = app.i18n.locale === 'ca' ? 'qui-som' : 'quienes-somos'
+    const page = await $content(`${defaultLocale}/pages/${title}`).fetch()
+
+    return {
+      page,
+    }
+  },
+}
+</script>
+
+<style></style>
