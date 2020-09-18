7a85008df63556f6ab9207cffb13fb525520eb35
Josep Monjo <jvmonjo@gmail.com>
2020-09-18 11:45:16 +0200

    FIXED: env variables in netlify

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,17 @@ export default {
     trailingSlash: true,
   },
 
+  env: {
+    BASE_URL: process.env.BASE_URL,
+    FB_API_KEY: process.env.FB_API_KEY,
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_DATABASE_URL: process.env.FB_DATABASE_URL,
+    FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+    FB_STORAGE_BUKET: process.env.FB_STORAGE_BUKET,
+    FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+    FB_APP_ID: process.env.FB_APP_ID,
+  },
+
   buildModules: ['@nuxtjs/dotenv'],
   /*
    ** Headers of the page
