efd51c6425c23a38712c747ecb7d2e2f95109f47
monjo <jvmonjo@gmail.com>
2020-09-28 11:16:11 +0200

    changed bulma import to the bottom

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 export default function ({ store, redirect, app }) {
   // If the user is not authenticated
-  if (!store.state.token) {
-    const lang = app.i18n.locale
-    return redirect(`/${lang}/login/`)
-  }
+  // if (!store.state.token) {
+  //   const lang = app.i18n.locale
+  //   return redirect(`/${lang}/login/`)
+  // }
 }
